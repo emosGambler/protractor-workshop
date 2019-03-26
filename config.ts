86a4882108bf466af546d27cb94e3f2ec94ec29f
Jacek Góraj <jckgrj@gmail.com>
2019-03-26 22:03:13 +0100

    logging as different user using scripts

--- config.ts
+++ config.ts
@@ -1,11 +1,15 @@
 import { Config } from "protractor";
 import { consoleReporter } from "./spec/support/reporter";
+import { DEFAULT_USERNAME } from "./spec/support/data";
 
 export let config: Config = {
     baseUrl: "http://www.way2automation.com/angularjs-protractor/banking/#/login",
     directConnect: true,
     capabilities: {
         browserName: 'chrome'
+    },
+    params: {
+        username: DEFAULT_USERNAME
     },
     specs: [
         "spec/**/*.spec.js"
